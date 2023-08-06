638cc2859e18a92ea47f6bce494f989481cbfa6b
SatanshuMishra <satanshumishra@outlook.com>
2023-08-06 15:38:55 -0700

    Build 1.0 | Features Update Completed Basic Implementation

---
+++ SimpleTodoApp/SimpleTodoApp/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SimpleTodoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddTask(object sender, RoutedEventArgs e)
+        {
+            string taskText = TaskInput.Text;
+
+            if (!string.IsNullOrEmpty(taskText))
+            {
+                TextBlock textBlock = new TextBlock
+                {
+                    Text = taskText,
+                    Margin = new Thickness(10),
+                };
+                TaskList.Children.Add(textBlock);
+                TaskInput.Clear();
+            }
+        }
+    }
+}
